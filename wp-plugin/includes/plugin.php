eaafff4e6d2bb0a9f405b6c78cf6df8e66b5c9c5
mrbek1 <modaminovodilbek@gmail.com>
2026-01-12 22:42:15 +0500

    Инициализация плагина

---
+++ wp-plugin/includes/plugin.php
@@ -0,0 +1,19 @@
+<?php
+add_action("admin_menu", "main_menu_page", 25);
+
+function main_menu_page(){
+    add_menu_page(
+        'onSET Trainer',
+        'onSET',
+        'manage_options',
+        'onset',
+        'main_page_preview',
+        'data:image/svg+xml; base64, ' . base64_encode('<svg width="20" height="20" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg"><path d="M896 1191l512-295v591l-512 296v-592zm-512-295v591l512-296zm512-887v591l-512 296v-591zm0 591l512-295v591z"/></svg>'),
+        20
+    );
+}
+function main_page_preview(){
+    echo "Обзор";
+}
+
+?>
